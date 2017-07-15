7e1ab587e2fb5b5c526ef8f9abcc7890191cf9b6
hdbullar <hdbullar@iupui.edu>
2017-07-15 11:12:45 -0400

    added file

---
+++ PasswordTest.py
@@ -0,0 +1,10 @@
+passwordFile = open('Malware_Project.docx')
+secretPassword = passwordFile.read()
+print('Enter your password')
+typedPassword = input()
+if typedPassword == secretPassword:
+    print('Access granted')
+    if typedPassword == '12345':
+        print('That password is the one that idiot put on their luggage!')
+    else:
+            print("Access Denied')
